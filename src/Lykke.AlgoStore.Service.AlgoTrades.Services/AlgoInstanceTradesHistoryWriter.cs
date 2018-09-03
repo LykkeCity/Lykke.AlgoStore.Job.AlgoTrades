d8e27d1901f69ae7dc94575f2ded7ba03c2546ec
Miladin Joksic <miladin.joksic@primeholding.com>
2018-09-03 15:23:53 +0200

    updated Models package

--- src/Lykke.AlgoStore.Service.AlgoTrades.Services/AlgoInstanceTradesHistoryWriter.cs
+++ src/Lykke.AlgoStore.Service.AlgoTrades.Services/AlgoInstanceTradesHistoryWriter.cs
@@ -6,6 +6,7 @@ using Lykke.Service.OperationsRepository.Contract.Cash;
 using Lykke.Service.OperationsRepository.Contract.History;
 using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Lykke.MatchingEngine.Connector.Models.Events;
 using OrderType = Lykke.MatchingEngine.Connector.Models.Events.OrderType;
@@ -27,8 +28,7 @@ namespace Lykke.AlgoStore.Service.AlgoTrades.Services
         /// <param name="historyRecord">Historical data which comes from RabbitMQ exchange</param>
         public async Task SaveAsync(OperationsHistoryMessage historyRecord)
         {
-
-            var operationType = (OperationType)Enum.Parse(typeof(OperationType), historyRecord.OpType);
+            var operationType = (OperationType) Enum.Parse(typeof(OperationType), historyRecord.OpType);
 
             if (operationType == OperationType.ClientTrade)
             {
@@ -37,7 +37,9 @@ namespace Lykke.AlgoStore.Service.AlgoTrades.Services
                 AlgoInstanceTrade algoInstanceOrder = null;
 
                 if (!string.IsNullOrEmpty(clientTrade.MarketOrderId))
-                    algoInstanceOrder = await _algoInstanceTradeRepository.GetAlgoInstanceOrderAsync(clientTrade.MarketOrderId, clientTrade.ClientId);
+                    algoInstanceOrder =
+                        await _algoInstanceTradeRepository.GetAlgoInstanceOrderAsync(clientTrade.MarketOrderId,
+                            clientTrade.ClientId);
 
                 if (algoInstanceOrder != null)
                 {
@@ -63,15 +65,44 @@ namespace Lykke.AlgoStore.Service.AlgoTrades.Services
         public async Task SaveAsync(Order order)
         {
             //Check if this is a limit order
-            if(order.OrderType != OrderType.Limit)
+            //REMARK: We will deal just with LIMIT orders in this method (for now)
+            //In future, we have a task for this, we will move market limit orders login in here too
+            //(we will stop to use orders history for market orders and make them use new ME)
+            if (order.OrderType != OrderType.Limit)
                 return;
 
-            //REMARK: Check in MEA how do we save limit orders and how we can identity it
-            //similar to the code from above _algoInstanceTradeRepository.GetAlgoInstanceOrderAsync
+            var algoInstanceOrder =
+                await _algoInstanceTradeRepository.GetAlgoInstanceOrderAsync(order.Id, order.WalletId);
 
-            //TODO: Map order to AlgoInstanceTrade
-            var trade = new AlgoInstanceTrade();
-            await _algoInstanceTradeRepository.SaveAlgoInstanceTradeAsync(trade);
+            //If this is not AlgoStore order just ignore it
+            if (algoInstanceOrder == null)
+                return;
+
+            //If order does not have any trades then ignore it :)
+            if (!order.Trades.Any())
+                return;
+
+            foreach (var orderTrade in order.Trades)
+            {
+                //Map ME order to AlgoInstanceTrade
+                var trade = new AlgoInstanceTrade
+                {
+                    InstanceId = algoInstanceOrder.InstanceId,
+                    AssetId = orderTrade.BaseAssetId,
+                    AssetPairId = order.AssetPairId,
+                    Fee = orderTrade.Fees.Any()
+                        ? orderTrade.Fees.Where(x => !string.IsNullOrEmpty(x.Volume)).Sum(x => double.Parse(x.Volume))
+                        : (double?) null,
+                    Amount = string.IsNullOrEmpty(orderTrade.BaseVolume) ? (double?)null : double.Parse(orderTrade.BaseVolume),
+                    WalletId = algoInstanceOrder.WalletId,
+                    OrderId = algoInstanceOrder.OrderId,
+                    IsBuy = algoInstanceOrder.IsBuy,
+                    Price = algoInstanceOrder.Price,
+                    DateOfTrade = orderTrade.Timestamp
+                };
+
+                await _algoInstanceTradeRepository.SaveAlgoInstanceTradeAsync(trade);
+            }
         }
     }
 }
